00d451f4d05723b9e3b2874a0dbb3138ee7037db
George Bryant <george.s.bryant@gmail.com>
2018-05-17 09:07:23 +0100

    Re-write the way that card values are calculated to use maps and getters instead of for loops

--- BlackjackDeck.js
+++ BlackjackDeck.js
@@ -18,6 +18,8 @@ export default class BlackjackDeck {
       });
   }
 
+  // TODO: Rewrite this to push new cards directly to the player's hand and then
+  // call handTotals getter as per comments in player file
   dealCard(player) {
     fetch(this.apiUrl + 'deck/' + this.deckId + '/draw/?count=1')
       .then(response => response.json())
--- BlackjackPlayer.js
+++ BlackjackPlayer.js
@@ -1,39 +1,37 @@
 export default class BlackjackPlayer {
- // TODO player: hand, handTotal, winCount, stand, request cards, flag: bust,
- // TODO dealer: shuffle deck, hand, handTotal, request cards, stands, isDealer flag
+  // TODO player: hand, handTotal, winCount, stand, request cards, flag: bust,
+  // TODO dealer: shuffle deck, hand, handTotal, request cards, stands, isDealer flag
 
- constructor() {
-   this.hand = [];
-   this.handTotals = [0];
-   this.bust = false;
-   this.win = false;
- }
+  constructor() {
+    this.hand = [];
+    this.bust = false;
+    this.win = false;
+  }
 
- addCardToHand(card) {
-   //If the card is an ace, duplicate the array of totals and add 1 to the original
-   //array and add 11 to the duplicated, add values if they dont already exist
-   if(card.value === 'ACE') {
-     let duplicateHandTotals = this.handTotals;
-     for (let total of this.handTotals) {
-       total += 1;
-       console.log(total);
-     }
-     for (let total of duplicateHandTotals) {
-       total += 11;
-       console.log(total);
-       if (!this.handTotals.includes(total)) this.handTotals.push(total);
-     }
-   } else if (['JACK', 'QUEEN', 'KING'].includes(card.value)) {
-     for(let total of this.handTotals) {
-       //Face cards are always worth 10;
-       total += 10;
-       console.log(total);
-     }
-   } else {
-     for(let total of this.handTotals) {
-       total += parseInt(card.value);
-       console.log(total);
-     }
-   }
- }
+  get handTotals() {
+    this.hand.sort();
+    let handTotals = [0];
+
+    this.hand.map(card => {
+      if (['JACK', 'QUEEN', 'KING'].includes(card.value)) {
+        //Face cards are always worth 10
+        handTotals = handTotals.map(total => total + 10);
+
+      } else if(Number.isInteger(card.value))) {
+        const { value } = card;
+        const cardValue = parseInt(value, 10);
+
+        handTotals = handTotals.map(total => total + cardValue);
+      } else if (card.value === 'ACE'){
+        handTotals = handTotals.map(total => total + 1);
+        // TODO:
+        //Calculate these last
+        //Duplicate the current number of hand totals
+        //Add 1 to the original hand totals, add 11 to the duplications
+        //Combine the arrays
+        //Remove the duplicates
+      }
+
+    })
+  }
 }
