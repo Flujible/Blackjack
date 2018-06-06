caf714e92e74554f86c734430e83ac54304d189d
George Bryant <george.s.bryant@gmail.com>
2018-06-06 18:51:36 +0100

    Split functions out into smaller functions

--- BlackjackDeck.js
+++ BlackjackDeck.js
@@ -32,50 +32,61 @@ export default class BlackjackDeck {
       .then((data) => {
         this.lastDrawnCard = data.cards[0].code;
         player.hand.push(data.cards[0]);
-        let cards;
-        let totals;
-        player.hand.forEach((card, index) => {
-          if (index === 0) {
-            cards = card.code;
-          } else {
-            cards += ', '+card.code;
-          }
-        });
-        player.handTotals.forEach((total, index) => {
-          if (index === 0) {
-            totals = total;
-          } else if(total <= 21) {
-            totals += ', ' + total;
-          }
-        });
-        if(player.isDealer) {
-          document.getElementById('dealerHand').innerText = "Dealer's hand: " + cards;
-          document.getElementById('dealerTotals').innerText = "Dealer's hand totals: " + totals;
-        } else {
-          document.getElementById('playerHand').innerText = "Your hand: " + cards;
-          document.getElementById('playerTotals').innerText = "Your hand totals: " + totals;
-        }
-
-        console.log(player.handTotals);
-        if (player.handTotals.every(value => value > 21)) {
-          document.getElementById('drawButton').disabled = true
-          document.getElementById('standButton').disabled = true;
-          this.createGameEndMessage("You lose 😭");
-        }
-        if (player.handTotals.includes(21)) {
-          if (player.isDealer) {
-            document.getElementById('drawButton').disabled = true;
-            document.getElementById('standButton').disabled = true;
-            this.createGameEndMessage("Dealer wins 😭");
-          } else {
-            document.getElementById('drawButton').disabled = true;
-            document.getElementById('standButton').disabled = true;
-            this.createGameEndMessage("You win! 😄");
-          }
-        }
+        this.updatePlayerData(player);
+        this.evaluateHand(player);
       }).catch((err) => {
         console.log('Request failed', err);
       });
+  }
+
+  updatePlayerData(player) {
+    let cards;
+    let totals;
+    player.hand.forEach((card, index) => {
+      if (index === 0) {
+        cards = card.code;
+      } else {
+        cards += ', ' + card.code;
+      }
+    });
+    if(player.isDealer) {
+      document.getElementById('dealerHand').innerText = "Dealer's hand: " + cards;
+    } else {
+      document.getElementById('playerHand').innerText = "Your hand: " + cards;
+    }
+
+    //Add totals to the list of totals unless the total is >21
+    player.handTotals.forEach((total, index) => {
+      if (index === 0) {
+        totals = total;
+      } else if(total <= 21) {
+        totals += ', ' + total;
+      }
+    });
+    if(player.isDealer) {
+      document.getElementById('dealerTotals').innerText = "Dealer's hand totals: " + totals;
+    } else {
+      document.getElementById('playerTotals').innerText = "Your hand totals: " + totals;
+    }
+  }
+
+  evaluateHand(player) {
+    if (player.handTotals.every(value => value > 21)) {
+      document.getElementById('drawButton').disabled = true
+      document.getElementById('standButton').disabled = true;
+      this.createGameEndMessage("You lose 😭");
+    }
+    if (player.handTotals.includes(21)) {
+      if (player.isDealer) {
+        document.getElementById('drawButton').disabled = true;
+        document.getElementById('standButton').disabled = true;
+        this.createGameEndMessage("Dealer wins 😭");
+      } else {
+        document.getElementById('drawButton').disabled = true;
+        document.getElementById('standButton').disabled = true;
+        this.createGameEndMessage("You win! 😄");
+      }
+    }
   }
 
   createGameEndMessage(innerText) {
