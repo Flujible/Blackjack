03591007c7a2dad6f0dae974a8b4b7db2dc85468
George Bryant <george.bryant@mirumagency.com>
2018-11-24 18:03:26 +0000

    Rename and simplify methods

--- Player.js
+++ Player.js
@@ -1,4 +1,4 @@
-export default class BlackjackPlayer {
+export default class Player {
   // TODO player: stand
   // TODO dealer: auto-play when player stands
 
@@ -12,22 +12,12 @@ export default class BlackjackPlayer {
   //Re-enable buttons and reset text nodes for the start of a new game
   reset() {
     this.hand = [];
-    document.getElementById('drawButton').disabled = false;
-    document.getElementById('standButton').disabled = false;
-    if(this.isDealer) {
-      document.getElementById('dealerHand').innerText = "Dealer's hand: ";
-      document.getElementById('dealerTotals').innerText = "Dealer's hand totals: ";
-    } else {
-      document.getElementById('playerHand').innerText = "Your hand: ";
-      document.getElementById('playerTotals').innerText = "Your hand totals: ";
-    }
   }
 
   
   get handTotals() {
     let handTotals = [0];
     let duplicateTotals;
-    let aces = 0;
 
     //For each card in the player's hand, convert its face number to a value and
     //add it to the player's totals
@@ -35,9 +25,10 @@ export default class BlackjackPlayer {
       if (Number.isInteger(parseInt(card.value))) {
         const { value } = card;
         const cardValue = parseInt(value, 10);
+        //Add that value to every available hand total
         handTotals = handTotals.map(total => total + cardValue);
       } else if (['JACK', 'QUEEN', 'KING'].includes(card.value)){
-        //Face cards are always worth 10
+        //Face cards are always worth 10, add this to hand totals
         handTotals = handTotals.map(total => total + 10);
       } else if (card.value === 'ACE'){
         // Duplicate the hand totals, add 1 to the original and 11 to the
@@ -52,22 +43,5 @@ export default class BlackjackPlayer {
       }
     });
     return handTotals;
-  }
-
-  dealerPlays() {
-    /**
-    When the dealer has served every player, his face-down card is turned up.
-
-    If the total is 17 or more, he must stand.
-
-    If the total is 16 or under, he must take a card.
-
-    He must continue to take cards until the total is 17 or more, at which point the dealer must stand.
-
-    If the dealer has an ace, and counting it as 11 would bring his total to 17
-    or more (but not over 21), he must count the ace as 11 and stand.
-    The dealer's decisions, then, are automatic on all plays, whereas the player
-    always has the option of taking one or more cards.
-    */
   }
 }
