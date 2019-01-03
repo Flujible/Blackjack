ce7beb4996a7ef176c782b3f5b1064d3a71da733
George Bryant <george.bryant@mirumagency.com>
2019-01-03 21:53:56 +0000

    Refactoring functions

--- Player.js
+++ Player.js
@@ -12,55 +12,67 @@ export default class Player {
    * @desc Updates the player's hand totals, hand string, and totals string
    */
   updatePlayerData() {
-    //TODO: Move to bespoke function
-    this.hand.map((card, index) => {
-      index === 0 ? this.handString = card.code : this.handString += `, ${card.code}`;
-    });
+    let cardCodes = this.hand.map(card => card.code);
+    this.handString = this.stringify(cardCodes);
+    this.handTotals = this.calcHandTotals();
+    this.totalsString = this.stringify(this.handTotals);
 
-    //For each card in the player's hand, convert its face number to a value and
-    //add it to the player's totals
-    //TODO: hand totals is returning undefined because nothing is ever being pushed to it
-    // This function is more complex than expected, move to its own function and call from here
-    let duplicateTotals;
-    this.hand.map(card => {
-      if (parseInt(card.value)) {
-        const cardValue = parseInt(card.value);
-        this.handTotals.map(total => total + cardValue);
-      } else if (['JACK', 'QUEEN', 'KING'].includes(card.value)){
-        //Face cards are always worth 10, add this to hand totals
-        this.handTotals.map(total => total + 10);
-      } else if (card.value === 'ACE'){
-        // Duplicate the hand totals, add 1 to the original and 11 to the
-        // other, dont add duplicates
-        duplicateTotals = this.handTotals.slice();
-        this.handTotals.map(total => total + 1);
-        duplicateTotals = duplicateTotals.map(total => this.handTotals.push(total + 11));
-      }
-    });
 
     //TODO: Move to bespoke function
-    //Remove any duplicates or values > 21 from the hand totals
-    this.handTotals = [...new Set(this.handTotals)]
-    for(let i = this.handTotals.length; i === 0; i--) {
-      this.handTotals[i] > 21 ? this.handTotals.splice(i, 1) : '';
-    }
-
-    this.handTotals.map((total, index) => {
-      index === 0 ? this.totalsString = total : this.totalsString += `, ${total}`;
-    });
-    
-
-    //TODO: Move to bespoke function
-    this.isDealer ? 
-      document.getElementById('dealerHand').innerText = "Dealer's hand: " + this.handString : 
+    this.isDealer ?
+      document.getElementById('dealerHand').innerText = "Dealer's hand: " + this.handString :
       document.getElementById('playerHand').innerText = "Your hand: " + this.handString;
-    this.isDealer ? 
-      document.getElementById('dealerTotals').innerText = "Dealer's hand totals: " + this.totalsString : 
+    this.isDealer ?
+      document.getElementById('dealerTotals').innerText = "Dealer's hand totals: " + this.totalsString :
       document.getElementById('playerTotals').innerText = "Your hand totals: " + this.totalsString;
   }
 
   evaluateHand() {
     return this.handTotals.every(value => value > 21 ? -1 :
     this.handTotals.includes(21) ? 1 : 0)
+  }
+
+  calcHandTotals() {
+    //For each card in the player's hand, convert its face number to a value and
+    //add it to the player's totals
+    let handTotals = [0];
+    let duplicateTotals;
+    this.hand.map(card => {
+      if (parseInt(card.value)) {
+        const cardValue = parseInt(card.value);
+        handTotals = handTotals.map(total => total + cardValue);
+      } else if (['JACK', 'QUEEN', 'KING'].includes(card.value)){
+        //Face cards are always worth 10, add this to hand totals
+        handTotals = handTotals.map(total => total + 10);
+      } else if (card.value === 'ACE'){
+        // Duplicate the hand totals, add 1 to the original and 11 to the
+        // other, dont add duplicates
+        duplicateTotals = handTotals.slice();
+        handTotals = handTotals.map(total => total + 1);
+        duplicateTotals = duplicateTotals.map(total => handTotals.push(total + 11));
+      }
+    });
+
+    //TODO: This isnt always being run for some reason
+    let cleanHandTotals = [...new Set(handTotals)];
+    for(let i = cleanHandTotals.length -1; i === 0; i--) {
+      console.log(cleanHandTotals[i])
+      if(cleanHandTotals[i] > 21) {
+        console.log("over 21 found: "+cleanHandTotals[i]);
+        cleanHandTotals.splice(i, 1);
+      }
+
+    }
+    console.log(`Hand: ${cleanHandTotals}`);
+    return cleanHandTotals;
+  }
+
+
+  stringify(arr) {
+    let string;
+    arr.map((item, index) => {
+      index === 0 ? string = item : string += `, ${item}`;
+    })
+    return string;
   }
 }
