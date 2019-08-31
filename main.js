583a236bdcea3b31476e1f76e4379641bb77e247
George Bryant <george.s.bryant@gmail.com>
2019-08-31 18:20:13 +0100

    Set button functions before starting game

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let apiUrl = 'https://deckofcardsapi.com/api/';
 let game = new Game(document, apiUrl);
 
 window.onload = () => {
-  game.startGame();
   document.getElementById('drawButton').onclick = () => game.dealCardToPlayer();
   document.getElementById('resetButton').onclick = () => game.startGame();
   document.getElementById('standButton').onclick = () => game.startDealersTurn();
   document.getElementById('giveDealerAce').onclick = () => game.giveDealerAce();
+  game.startGame();
 }
